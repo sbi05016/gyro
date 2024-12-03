7a539d080457a20f443d605b4693103a2424fb2d
sbi05016 <hyj2017106098@google.com>
2024-12-03 15:05:16 +0900

    'a'

--- gyro.py
+++ gyro.py
@@ -141,12 +141,17 @@ def paging(data):
         st.warning("데이터가 없습니다.")
         return
     
+    elif data.shape[0] == 1:
+        items_per_page = 1
+        return
+    
     elif data.shape[0] < 10:
         st.sidebar.warning("데이터가 너무 작아 페이지당 항목 수를 설정할 수 없습니다.")
         items_per_page = data.shape[0]  # 모든 데이터를 한 페이지로 표시
     
-    default_items_per_page = max(1, data.shape[0] // 100)
-    items_per_page = st.sidebar.slider("페이지당 항목 수", min_value=1, max_value=data.shape[0], value=min(default_items_per_page,data.shape[0]), step=10)
+    else:
+        default_items_per_page = max(1, data.shape[0] // 100)
+        items_per_page = st.sidebar.slider("페이지당 항목 수", min_value=1, max_value=data.shape[0], value=min(default_items_per_page,data.shape[0]), step=10)
     
     total_items = len(data)
     
